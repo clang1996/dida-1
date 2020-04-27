bc62251ea50a2f8fc7cb8340ecc22acc9e31f21a
cl1996 <1430125208@qq.com>
2020-04-27 22:00:39 +0800

    收集四个组件数据到record

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,25 +1,56 @@
 <template>
   <Layout class-prefix="layout">
-    <Tags :data-source.sync="tags"/>
-    <Notes/>
-    <Types/>
-    <NumberPad/>
+    {{record}}
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
+    <Notes @update:value="onUpdateNotes"/>
+    <Types @update:value="onUpdateType"/>
+    <NumberPad @update:value="onUpdateAmount"/>
   </Layout>
 </template>
-<script lang="js">
-  import Tags from '@/components/Money/Tags.vue'
-  import NumberPad from '@/components/Money/NumberPad.vue'
-  import Types from '@/components/Money/Types.vue'
-  import Notes from '@/components/Money/Notes.vue'
+<script lang="ts">
+  import Vue from 'vue';
+  import Tags from '@/components/Money/Tags.vue';
+  import NumberPad from '@/components/Money/NumberPad.vue';
+  import Types from '@/components/Money/Types.vue';
+  import Notes from '@/components/Money/Notes.vue';
+  import {Component} from 'vue-property-decorator';
 
-  export default {
-    name: 'Money',
-    components: {Tags, Notes, Types, NumberPad},
-    data() {
-      return {
-        tags: ['衣','食']
-      }
-    },
+  type Record = {
+    tags: string[];
+    notes: string;
+    type: string;
+    amount: number;
+  }
+  @Component({
+    components: {Tags, Notes, Types, NumberPad}
+  })
+  export default class Money extends Vue {
+    tags = ['衣', '食'];
+    record: Record = {
+      tags: [],
+      notes: '',
+      type: '-',
+      amount: 0,
+    };
+
+    onUpdateTags(value: string[]) {
+      this.record.tags = value;
+    }
+
+    onUpdateNotes(value: string) {
+      this.record.notes = value;
+
+    }
+
+    onUpdateType(value: string) {
+      this.record.type = value;
+
+    }
+
+    onUpdateAmount(value: string) {
+      this.record.amount = parseFloat(value);
+
+    }
   }
 </script>
 <style lang="scss">
