be03c4ff98aaf7eae8ec1c282a62b0e8cee8e72b
cl1996 <1430125208@qq.com>
2020-05-02 21:50:48 +0800

    新增edit页面

--- src/views/EditLable.vue
+++ src/views/EditLable.vue
@@ -5,10 +5,22 @@
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
+  import {tagListModel} from '@/models/tagListModel';
 
   @Component
-  export default class EditLable extends Vue {
-
+  export default class EditLabel extends Vue {
+    created() {
+      const id = this.$route.params.id;
+      tagListModel.fetch();
+      const tags = tagListModel.data;
+      const tag = tags.filter(t => t.id === id)[0];
+      if (tag) {
+        console.log(tag);
+      } else {
+        this.$route.params;
+        this.$router.replace('/404');
+      }
+    }
   }
 </script>
 
