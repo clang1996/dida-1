745ec9e67bf9e6ec2c7a3233a17296def90aec28
cl1996 <1430125208@qq.com>
2020-05-04 23:45:44 +0800

    将数据操作封装进store

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -17,13 +17,14 @@
   import Types from '@/components/Money/Types.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import {Component} from 'vue-property-decorator';
+  import {store} from '@/store/index2';
 
   @Component({
     components: {Tags, FormItem, Types, NumberPad}
   })
   export default class Money extends Vue {
-    tags = window.tagList;
-    recordList = window.recordList;
+    tags = store.tagList;
+    recordList = store.recordList;
     record: RecordItem = {
       tags: [],
       notes: '',
@@ -48,7 +49,7 @@
     }
 
     saveRecord() {
-    window.createRecord(this.record)
+   store.createRecord(this.record)
     }
 
   }
