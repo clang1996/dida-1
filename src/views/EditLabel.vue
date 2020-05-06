8530c342ba638b2906df5164fc7cd6d709298914
cl1996 <1430125208@qq.com>
2020-05-06 14:55:39 +0800

    Vuex重构EditLabels.vue

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -3,7 +3,7 @@
     <div class="navBar">
       <Icon class="leftIcon" name="left" @click="goBack"/>
       <span class="title">编辑标签</span>
-      <span class="rightIcon" />
+      <span class="rightIcon"/>
     </div>
     <div class="form-wrapper">
       <FormItem :value="tag.name"
@@ -21,40 +21,51 @@
   import {Component} from 'vue-property-decorator';
   import FormItem from '@/components/Money/FormItem.vue';
   import Button from '@/components/Button.vue';
+
   @Component({
-    components: {Button, FormItem}
-  })
-  export default class EditLabel extends Vue {
-    tag?: Tag = undefined;
-    created() {
-      // this.tag =
-      //TODO
-      // store.findTag(this.$route.params.id);
-      if (!this.tag) {
-        this.$router.replace('/404');
+    components: {Button, FormItem},
+    //   computed:{
+    //     tag(){
+    //       return this.$store.state.currentTag
+    //     }
+    //   }
+    })
+    export default class EditLabel extends Vue {
+      get tag() {
+        return this.$store.state.currentTag;
+      }
+
+      created() {
+        const id = this.$route.params.id;
+        this.$store.commit('setCurrentTag', id);
+        if (!this.tag) {
+          this.$router.replace('/404');
+        }
+      }
+
+      update(name: string) {
+        if (this.tag) {
+          //TODO
+          // store.updateTag(this.tag.id, name);
+        }
+      }
+
+      remove() {
+        if (this.tag) {
+          return;
+          //TODO
+          // if(store.removeTag(this.tag.id)){
+          //   this.$router.back();
+          // }else{
+          //   window.alert('删除失败')
+          // }
+        }
+      }
+
+      goBack() {
+        this.$router.back();
       }
     }
-    update(name: string) {
-      if (this.tag) {
-        //TODO
-        // store.updateTag(this.tag.id, name);
-      }
-    }
-    remove() {
-      if (this.tag) {
-        return
-        //TODO
-        // if(store.removeTag(this.tag.id)){
-        //   this.$router.back();
-        // }else{
-        //   window.alert('删除失败')
-        // }
-      }
-    }
-    goBack() {
-      this.$router.back();
-    }
-  }
 </script>
 
 <style lang="scss" scoped>
@@ -66,21 +77,26 @@
     display: flex;
     align-items: center;
     justify-content: space-between;
+
     > .title {
     }
+
     > .leftIcon {
       width: 24px;
       height: 24px;
     }
+
     > .rightIcon {
       width: 24px;
       height: 24px;
     }
   }
+
   .form-wrapper {
     background: white;
     margin-top: 8px;
   }
+
   .button-wrapper {
     text-align: center;
     padding: 16px;
