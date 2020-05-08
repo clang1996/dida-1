b21f2bd24cafff68e88a3254483ce2f4aeb22c9d
cl1996 <1430125208@qq.com>
2020-05-08 22:05:59 +0800

    引入day.js

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,19 +4,7 @@
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
     <ol>
       <li v-for="(group,index) in result" :key="index">
-        <h3 class="title">{{group.title}}</h3>
-        <ol>
-          <li v-for="item in group.items" :key="item.id"
-              class="record"
-          >
-            <span>{{tagString(item.tags)}}</span>
-            <span class="notes">{{item.notes}}</span>
-            <span>￥{{item.amount}} </span>
-          </li>
-        </ol>
-      </li>
-      <li v-for="(group,index) in result" :key="index">
-        <h3 class="title">{{group.title}}</h3>
+        <h3 class="title">{{beautify(group.title)}}</h3>
         <ol>
           <li v-for="item in group.items" :key="item.id"
               class="record"
@@ -30,27 +18,6 @@
     </ol>
   </Layout>
 </template>
-<style scoped lang="scss">
-  %item {
-    padding: 8px 16px;
-    line-height: 24px;
-    display: flex;
-    justify-content: space-between;
-    align-content: center;
-  }
-  .title {
-    @extend %item;
-  }
-  .record {
-    background: white;
-    @extend %item;
-  }
-  .notes {
-    margin-right: auto;
-    margin-left: 16px;
-    color: #999;
-  }
-</style>
 
 <script lang="ts">
   import Vue from 'vue';
@@ -58,6 +25,8 @@
   import Tabs from '@/components/Tabs.vue';
   import intervalList from '@/constants/intervalList';
   import recordTypeList from '@/constants/recordTypeList';
+  import dayjs from 'dayjs';
+
   @Component({
     components: {Tabs},
   })
@@ -65,9 +34,27 @@
     tagString(tags: Tag[]) {
       return tags.length === 0 ? '无' : tags.join(',');
     }
+
+    beautify(string: string) {
+      const day = dayjs(string);
+      const now = dayjs();
+      if (day.isSame(now, 'day')) {
+        return '今天';
+      } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+        return '昨天';
+      } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+        return '前天';
+      } else if (day.isSame(now, 'year')) {
+        return day.format('M月D日');
+      } else {
+        return day.format('YYYY年M月D日');
+      }
+    }
+
     get recordList() {
       return (this.$store.state as RootState).recordList;
     }
+
     get result() {
       const {recordList} = this;
       type HashTableValue = { title: string, items: RecordList[] }
@@ -79,9 +66,11 @@
       }
       return hashTable;
     }
+
     beforeCreate() {
       this.$store.commit('fetchRecords');
     }
+
     type = '-';
     interval = 'day';
     intervalList = intervalList;
@@ -90,16 +79,42 @@
 </script>
 
 <style scoped lang="scss">
+  %item {
+    padding: 8px 16px;
+    line-height: 24px;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+  }
+
+  .title {
+    @extend %item;
+  }
+
+  .record {
+    background: white;
+    @extend %item;
+  }
+
+  .notes {
+    margin-right: auto;
+    margin-left: 16px;
+    color: #999;
+  }
+
   ::v-deep {
     .type-tabs-item {
       background: white;
+
       &.selected {
         background: #C4C4C4;
+
         &::after {
           display: none;
         }
       }
     }
+
     .interval-tabs-item {
       height: 48px;
     }
