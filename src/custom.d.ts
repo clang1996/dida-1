98ce447bddc8efa47f396db82cca0fd0f6b6023b
cl1996 <1430125208@qq.com>
2020-05-08 20:25:35 +0800

    完成列表CSS

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -3,7 +3,7 @@ type RecordItem = {
   notes: string;
   type: string;
   amount: number;
-  createAt?: Date
+  createAt?: string
 }
 type Tag = {
   id: string
@@ -16,6 +16,11 @@ type TagListModel = {
   update: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
   remove: (id: string) => boolean
   save: () => void
+}
+type RootState = {
+  recordList: RecordItem[],
+  tagList: Tag[],
+  currentTag?: Tag
 }
 
 interface Window {
